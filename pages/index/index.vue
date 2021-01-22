c64381e96f681b511f4070d0ed81fbe561f1d50d
wh <1570058176@qq.com>
2021-01-22 17:02:05 +0800

    h5详情

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,6 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+	<view class="">
+		<image src="../../static/loading-white.gif" class="gif-white response" style="height:480rpx"></image>
 	</view>
 </template>
 
@@ -11,42 +8,56 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				sendimg: '',
+				token: '',
+				articleId: '',
+				type: ''
+			};
+		},
+		methods: {
+			// 获取参数信息
+			getQueryVariable(variable) {
+				//获取地址栏参数信息
+				var query = window.location.search.substring(1);
+				console.log('query参数', query) //id=1
+				// 已‘&’符号将query分割为数组
+				var vars = query.split('&');
+				console.log('vars', vars) // ["id=1"]
+				for (var i = 0; i < vars.length; i++) {
+					var pair = vars[i].split('=');
+					if (pair[0] == variable) {
+						return pair[1]; //返回数组第二个参数
+					}
+				}
+				console.log('pair', pair) //["id", "1"]
+				return false;
 			}
 		},
 		onLoad() {
-
-		},
-		methods: {
-
+			let that = this;
+			// that.type = that.getQueryVariable('type');
+			if (that.getQueryVariable('type') == 'advisoryDetails') { //热门咨询详情
+				uni.redirectTo({
+					url: '../advisoryDetails/advisoryDetails?id=' + that.getQueryVariable('id')
+				});
+			} else if (that.getQueryVariable('type') == 'officialDetails') { //官方发布
+				uni.redirectTo({
+					url: '../officialDetails/officialDetails?id=' + that.getQueryVariable('id')
+				});
+			}
 		}
-	}
+	};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+	.cu-avatar {
+		background-repeat: no-repeat;
+		background-size: 100% 100%;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	page {
+		padding-top: 100rpx;
+		background-color: #fff;
+		padding-bottom: 100rpx;
 	}
 </style>
