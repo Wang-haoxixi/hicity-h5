ecc585655a7c264cc945e72372509236627f650b
wh <1570058176@qq.com>
2021-01-22 17:13:59 +0800

    h5详情

---
+++ pages/404/404.vue
@@ -0,0 +1,26 @@
+<template>
+	<view style="display: flex;flex-direction: column;align-items: center;justify-content: center;height: 100vh;">
+		<view class="cu-avatar" style="background-image: url(../../static/404.png);width: 572rpx;height: 270rpx;"></view>
+		<!-- <image src="../../static/404.png" mode="" style="width: 572rpx;height: 270rpx;"></image> -->
+		<view style="color: #919397;font-size: 24rpx;margin-top: 20rpx;">您访问的资源不见了</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	page {
+		background-color: white;
+	}
+</style>
---
+++ pages/advisoryDetails/advisoryDetails.vue
@@ -0,0 +1,55 @@
+<template>
+	<!-- 热门咨询详情 -->
+	<view>
+		<jyf-parser class="parser" :html="html" :tag-style="tagStyle" lazy-load></jyf-parser>
+	</view>
+</template>
+
+<script>
+	import jyfParser from "@/components/jyf-Parser/jyf-parser";
+	export default {
+		components: {
+			jyfParser
+		},
+		data() {
+			return {
+				tagStyle: {
+					body: 'line-height: 1.8;',
+					img: 'background-size: contain|cover;width:100%;height:auto;'
+				},
+				html: ""
+			};
+		},
+		onLoad(option) {
+			console.log('热门咨询详情', option)
+			// 請求到文章详情
+			uni.request({
+				url: '/api/cms/open/news_details', //仅为示例，并非真实接口地址。
+				data: {
+					// newsId: option.id
+					newsId: 14
+				},
+				success: (res) => {
+					console.log('res', res);
+					if (res.data.code != 0) {
+						uni.redirectTo({
+							url: '../404/404'
+						});
+					}
+					this.html = res.data.data.data.content
+				}
+			});
+		}
+	}
+</script>
+
+<style lang="scss">
+	page {
+		background-color: #fff;
+	}
+
+	.parser {
+		padding: 0 40rpx;
+		margin-top: 40rpx;
+	}
+</style>
