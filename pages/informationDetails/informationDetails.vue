6a62641398768f3df043c783fc6308fd83351687
前端-宋星宇 <1124145848@qq.com>
2021-05-14 13:31:36 +0800

    热议详情创建

--- pages/informationDetails/informationDetails.vue
+++ pages/informationDetails/informationDetails.vue
@@ -5,7 +5,7 @@
 		<view class="detail-box" v-if="detail">
 			<view class="title">{{detail.title}}</view>
 			<view class="publish-time">发布时间：{{ gettime(detail.createTime) }}</view>
-			<jyf-parser class="parser" :html="getContent(detail.content)" :tag-style="tagStyle" lazy-load></jyf-parser>
+			<jyf-parser class="parser" :html="detail.content" :tag-style="tagStyle" lazy-load></jyf-parser>
 			<view class="browse-num">帖子浏览数：{{detail.browseNum}}</view>
 		</view>
 		<view class="noData" v-if="!detail">
